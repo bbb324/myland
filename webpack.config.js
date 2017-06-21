4558b480d6482f98b60ba24cc555ef79e3382178
bbb324 <xiejun325@gmail.com>
2017-06-22 00:23:37 +0800

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,6 @@ module.exports = {
       loader: 'eslint-loader'
     }],
     loaders: [
-      //.css 文件使用 style-loader 和 css-loader 来处理
-      {
-        test: /\.css$/,
-        loader: "style!css"
-      },
       //.js 文件使用 jsx-loader 来编译处理
       {
         test: /\.js$/,
@@ -23,7 +18,13 @@ module.exports = {
         query: {
           presets: ['es2015']
         }
-      }
+      }, {
+      test: /\.less$/,
+      loader: 'style!css!less'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css!less'
+    }
     ]
   },
   resolve: {
