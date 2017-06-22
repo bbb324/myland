d45e0123dc55da90cb00b4e6fbf1aea2ec679f93
bbb324 <xiejun325@gmail.com>
2017-06-23 01:37:10 +0800

    finish pc dev

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 class instance {
   init() {
     require('./autofill.js');
-    if(navigator.userAgent.indexOf('iPhone')!= -1 || navigator.userAgent.indexOf('Android')!= -1 ||
-      navigator.userAgent.indexOf('iPad')!= -1) {
+    if(navigator.userAgent.indexOf('iPhone')!= -1 || navigator.userAgent.indexOf('Android')!= -1 ) {
       require('../css/m-style.css');
     } else {
       require('../css/style.css');
